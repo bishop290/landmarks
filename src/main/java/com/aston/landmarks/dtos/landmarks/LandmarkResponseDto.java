6ff4190336cfb04ca8c1126eccb57c3b021002c9
Grigorii Kuznetsov <a925041@yandex.ru>
2024-09-28 11:49:36 +0300

    fix: Extra field

--- src/main/java/com/aston/landmarks/dtos/landmarks/LandmarkResponseDto.java
+++ src/main/java/com/aston/landmarks/dtos/landmarks/LandmarkResponseDto.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Setter;
 
-import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -16,7 +15,6 @@ public class LandmarkResponseDto {
     private final Long id;
     private final String name;
     private final String description;
-    private Date date;
     private final LocalDate dateCreated;
     private final String attraction;
     private final LocalityResponseDto locality;
